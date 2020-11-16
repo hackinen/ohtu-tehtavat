610eb9818c7efaae4e6862dd8b2d1924fa7cb6e1
Iira <iira.hakkinen@helsinki.fi>
2020-11-16 11:35:07 +0200

    tehtävä 10

--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -21,13 +21,56 @@ public class Stepdefs {
         driver.get(baseUrl);
         WebElement element = driver.findElement(By.linkText("login"));       
         element.click();   
-    }    
+    }
     
+    @Given("command new user is selected")
+    public void commandNewUserIsSelected() {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));       
+        element.click();   
+    }
+    
+
     @When("correct username {string} and password {string} are given")
     public void correctUsernameAndPasswordAreGiven(String username, String password) {
         logInWith(username, password);
-    }    
+    }
     
+    @When("nonexistent username {string} and password {string} are given")
+    public void incorrectUsernameAndPasswordAreGiven(String username, String password) {
+        logInWith(username,password);
+    }
+
+    @When("a valid username {string} and password {string} and matching password confirmation are entered")
+    public void validUsernameAndPaswordAndMatchingPasswordAreGiven(String username, String password) {
+        createUserWith(username, password, password);
+    }
+
+    @When("a too short username {string} and password {string} are given")
+    public void tooShortUsernameAndPasswordAreGiven(String username, String password) {
+        createUserWith(username, password, password);
+    }
+    
+    @When("a valid username {string} and too short password {string} are given")
+    public void validUsernameAndTooShortpasswordAreGiven(String user, String pass) {
+        createUserWith(user, pass, pass);
+    }
+
+    @When("a valid username {string} and valid password {string} and not matching password {string} are entered")
+    public void validUsernameValidPasswordWrongMatchingPasswordAreEntered(String u, String p1, String p2) {
+        createUserWith(u,p1,p2);
+    }
+
+    @Then("a new user is created")
+    public void newUserIsCreated() {
+        pageHasContent("Welcome to Ohtu Application!");
+    }
+
+    @Then("user is not created and error {string} is reported")
+    public void userNotCreatedAndErrorUsernameTooShortReported(String string) {
+        pageHasContent(string);
+    }
+
     @Then("user is logged in")
     public void userIsLoggedIn() {
         pageHasContent("Ohtu Application main page");
@@ -74,4 +117,16 @@ public class Stepdefs {
         element = driver.findElement(By.name("login"));
         element.submit();  
     } 
+
+    private void createUserWith(String username, String password, String password2) {
+        assertTrue(driver.getPageSource().contains("Create username and give password"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(password2);
+        element = driver.findElement(By.name("signup"));
+        element.submit();  
+    }
 }
